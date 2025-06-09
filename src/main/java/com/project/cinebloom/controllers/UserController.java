8e7c4c360d255e5840616688fc668076e1f5f418
Melania Ion <melaniaion24@gmail.com>
2025-06-09 17:29:33 +0300

    profile frontend

--- src/main/java/com/project/cinebloom/controllers/UserController.java
+++ src/main/java/com/project/cinebloom/controllers/UserController.java
@@ -60,6 +60,7 @@ public class UserController {
         } catch (Exception e) {
             model.addAttribute("ProfileError", e.getMessage());
         }
+        
         return "profile";
     }
 
